2391962d2358ce8048975f87853eed8710bc85f1
Michał Oręziak <micorix.dev@gmail.com>
2023-05-10 10:00:18 +0200

    Configure deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  // TODO(micorix): Either base on env vars or remove
+  site: 'https://micorix.github.io',
+  base: '/cyber-security-club-website-prototype',
   integrations: [tailwind()]
 });
