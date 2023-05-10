d7d57847e9219374b15c808585ce62fa103046a1
Michał Oręziak <micorix.dev@gmail.com>
2023-05-10 10:07:54 +0200

    Remove path prefix from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // TODO(micorix): Either base on env vars or remove
-  site: 'https://micorix.github.io',
-  base: '/cyber-security-club-website-prototype',
   integrations: [tailwind()]
 });
